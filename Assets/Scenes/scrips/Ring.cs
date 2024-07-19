6c88bf804a5c9a2ab521b4f922bf8b34f043f0e1
Falloke <tossapontansupan9@gmail.com>
2024-07-19 17:01:49 +0700

    f

---
+++ Assets/Scenes/scrips/Ring.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    // Instantiates prefabs in a circle formation
+
+    public GameObject prefab;
+    public int numberOfObjects = 20;
+    public float radius = 5f;
+
+    void Ringgy()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            float x = Mathf.Cos(angle) * radius;
+            float z = Mathf.Sin(angle) * radius;
+            Vector3 pos = transform.position + new Vector3(x, 0, z);
+            float angleDegrees = -angle*Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+            Instantiate(prefab, pos, rot);
+        }
+    }
+      void Start()
+    {
+        this.Ringgy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+    // Start is called before the first frame update
+  
+
